b4c29ea754990f14c46fe99428b7bfb9c3f65540
dongilpak <ehddlf2395@gmail.com>
2023-08-25 14:27:51 +0900

    반복함수 유틸폴더에 저장 및 common css 수정

--- src/Components/ListOfLotto/valueOfLotto.tsx
+++ src/Components/ListOfLotto/valueOfLotto.tsx
@@ -1,38 +1,7 @@
 import React from 'react';
+import { checkNumPart } from '../../utils/CheckNumPart';
 
 const valueOfLotto = (title: string, value: number[], i: number) => {
-    const checkNumPart = (num: number) => {
-        const numStorage = [
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-            [21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
-            [31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
-            [41, 42, 43, 44, 45],
-        ];
-
-        for (let i = 0; i < numStorage.length; i++) {
-            const part = numStorage[i].find(item => item === num)
-                ? `${i}`
-                : null;
-
-            switch (part) {
-                case '0':
-                    return 'part-one';
-                case '1':
-                    return 'part-two';
-                case '2':
-                    return 'part-three';
-                case '3':
-                    return 'part-four';
-                case '4':
-                    return 'part-five';
-
-                default:
-                    break;
-            }
-        }
-    };
-
     return (
         <ul className={`${title}--lotto-ball`} key={i}>
             <li className={`${title}--ball ${checkNumPart(value[0])}`}>
--- src/Components/LottoResult/LottoResult.tsx
+++ src/Components/LottoResult/LottoResult.tsx
@@ -3,44 +3,13 @@ import './LottoResult.css';
 import AddRoundedIcon from '@mui/icons-material/AddRounded';
 import { asyncGetResult } from '../../reducers/resultReducer';
 import { useAppSelector, useAppDispatch } from '../../hooks';
+import { checkNumPart } from '../../utils/CheckNumPart';
 
 const LottoResult = () => {
     const dispatch = useAppDispatch();
     const [updateNum, setUpdateNum] = useState(0);
 
     const lottoValue = useAppSelector(state => state.result.value);
-
-    const checkNumPart = (num: number) => {
-        const numStorage = [
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-            [21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
-            [31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
-            [41, 42, 43, 44, 45],
-        ];
-
-        for (let i = 0; i < numStorage.length; i++) {
-            const part = numStorage[i].find(item => item === num)
-                ? `${i}`
-                : null;
-
-            switch (part) {
-                case '0':
-                    return 'part-one';
-                case '1':
-                    return 'part-two';
-                case '2':
-                    return 'part-three';
-                case '3':
-                    return 'part-four';
-                case '4':
-                    return 'part-five';
-
-                default:
-                    break;
-            }
-        }
-    };
 
     const getUpdate = useCallback(() => {
         const standardDate = Number(new Date('2002-12-07'));
