448671e4afaa0dc0da5467176b7ab7a597ff68d2
dongilpak <ehddlf2395@gmail.com>
2023-09-02 17:08:14 +0900

    모바일 로또 이미지 표현

--- src/Components/ShowSaveList/ShowSaveList.tsx
+++ src/Components/ShowSaveList/ShowSaveList.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import './ShowSaveList.css';
 import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
 import ValueOfLotto from '../ValueOfLotto/valueOfLotto';
@@ -13,9 +13,9 @@ interface isOpen {
 }
 
 const ShowSaveList = ({ on, openListControl }: isOpen) => {
-    const [isCapture, setIsCapture] = useState<boolean>(false);
-
-    const captureRef = useRef(null);
+    const captureRef = useRef<HTMLDivElement>(null);
+    const saveBtnRef = useRef<HTMLButtonElement>(null);
+    const isMobile = navigator.userAgent.indexOf('Mobile') > -1;
 
     const dispatch = useAppDispatch();
 
@@ -25,61 +25,75 @@ const ShowSaveList = ({ on, openListControl }: isOpen) => {
         dispatch(removeLotto(i));
     };
 
-    const toggleHandler = () => {
-        setIsCapture(!isCapture);
-    };
-
     const downloadLotto = async () => {
         if (!captureRef.current) return;
 
         const spans = document.querySelectorAll<HTMLSpanElement>(
             'div.captureSection div.captureBox ul.lotto-ball-list li.ball-value > span'
         );
+        captureRef.current.style.display = 'block';
 
         for (let i = 0; i < spans.length; i++) {
             spans[i].style.height = '100%';
             spans[i].style.marginBottom = '10px';
         }
+
+        if (isMobile) {
+            document.querySelector<HTMLDivElement>(
+                '.captureSection > .captureBox'
+            ).style.display = 'flex';
+        }
+
         try {
             const div = captureRef.current;
 
             const canvas = await html2canvas(div);
-            canvas.toBlob(blob => {
-                if (blob !== null) {
-                    saveAs(blob, 'lotto.png');
-                }
-            });
+
+            if (isMobile) {
+                document.querySelector<HTMLDivElement>(
+                    '.captureSection > .captureBox'
+                ).style.display = 'none';
+
+                saveBtnRef.current.style.display = 'none';
+
+                const imgSrc = canvas.toDataURL('image/png');
+                const img = document.createElement('img');
+                img.src = imgSrc;
+                img.style.width = '100%';
+                img.style.height = '100%';
+
+                div.appendChild(img);
+            } else {
+                canvas.toBlob(blob => {
+                    if (blob !== null) {
+                        saveAs(blob, 'lotto.png');
+                    }
+                });
+                captureRef.current.style.display = 'none';
+            }
         } catch (error) {
             console.error('Error converting div to image:', error);
         }
+    };
 
-        setIsCapture(!isCapture);
+    const controlDisplayListBox = () => {
+        openListControl();
+
+        if (isMobile) {
+            saveBtnRef.current.style.display = 'inline-block';
+
+            const img = document.querySelector<HTMLCanvasElement>(
+                '.captureSection > img'
+            );
+
+            captureRef.current.removeChild(img);
+        }
     };
 
     return (
         <div className={`showSaveList ${on ? 'open' : 'close'}`}>
             <div className='showSaveBox'>
-                {isCapture ? (
-                    <>
-                        <div ref={captureRef} className='captureSection'>
-                            <CaptureList saveList={saveLottoValue} />
-                        </div>
-                        <div className='controlBtns'>
-                            <button
-                                onClick={toggleHandler}
-                                className='openControlBtn'
-                            >
-                                닫기
-                            </button>
-                            <button
-                                onClick={downloadLotto}
-                                className='saveControlBtn'
-                            >
-                                갤러리에 저장
-                            </button>
-                        </div>
-                    </>
-                ) : saveLottoValue.length === 0 ? (
+                {saveLottoValue.length === 0 ? (
                     <>
                         <p className='listNone'>저장된 목록 없음.</p>
                         <div className='controlBtns'>
@@ -99,17 +113,21 @@ const ShowSaveList = ({ on, openListControl }: isOpen) => {
 
                         <div className='controlBtns'>
                             <button
-                                onClick={openListControl}
+                                onClick={controlDisplayListBox}
                                 className='openControlBtn'
                             >
                                 닫기
                             </button>
                             <button
-                                onClick={toggleHandler}
+                                ref={saveBtnRef}
+                                onClick={downloadLotto}
                                 className='saveImageBtn'
                             >
-                                이미지로 보기
+                                {isMobile ? '이미지로 보기' : '이미지로 저장'}
                             </button>
+                        </div>
+                        <div ref={captureRef} className='captureSection'>
+                            <CaptureList saveList={saveLottoValue} />
                         </div>
                     </>
                 )}
